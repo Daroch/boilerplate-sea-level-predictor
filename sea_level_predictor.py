61df69406fee21e1b1c46e283bf06437d1b782ee
Daroch <daroch314@gmail.com>
2023-12-25 21:27:27 +0100

    creada aproximación lineal a los datos

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,9 +4,18 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
+    #df = df[df['Year'] >= 2000]
+    #df = df.dropna()
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
+    plt.scatter(x, y)
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.show()
 
 
     # Create first line of best fit
